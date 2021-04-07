017de35fbf40657f52e1a43a23deb8bb4de76c53
Noemi <noemi.rea@libero.it>
2021-04-07 16:00:10 +0200

    milestone 2

--- js/main.js
+++ js/main.js
@@ -5,20 +5,36 @@ var app = new Vue({
   data: {
     searchTitle: "",
     films: [],
+    series: [],
+    votes: [],
   },
 
   methods: {
     addtext: function () {
-      console.log(this.searchTitle);
       axios
         .get(
           "https://api.themoviedb.org/3/search/movie?api_key=60d77243c9ba552e841e00e4f6b5b02b&query=" +
             this.searchTitle
         )
         .then((result) => {
-          console.log(result.data.results);
           this.films = result.data.results;
-          console.log(this.films);
+          this.films.forEach((element) => {
+            element.vote_average = element.vote_average / 2;
+            element.vote_average = Math.ceil(element.vote_average);
+          });
+        });
+      axios
+        .get(
+          "https://api.themoviedb.org/3/search/tv?api_key=60d77243c9ba552e841e00e4f6b5b02b&query=" +
+            this.searchTitle
+        )
+        .then((result) => {
+          console.log(result.data.results);
+          this.series = result.data.results;
+          this.series.forEach((element) => {
+            element.vote_average = element.vote_average / 2;
+            element.vote_average = Math.ceil(element.vote_average);
+          });
         });
     },
   },
